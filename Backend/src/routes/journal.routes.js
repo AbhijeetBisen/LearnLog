e8a8a1d6720de8d6181b2e463410f3d95719402a
Abhijeet <a.bisen@op.iitg.ac.in>
2026-05-18 18:32:59 +0530

    Update environment variables and format journal routes

--- Backend/src/routes/journal.routes.js
+++ Backend/src/routes/journal.routes.js
@@ -11,10 +11,15 @@ const {
 const router = express.Router();
 
 router.post("/create-journal", createJournalController);
+
 router.put("/update-journal/:journalId", updateJournalController);
+
 router.delete("/:journalId", deleteJournalController);
+
 router.get("/allJournal", getAllJournalController);
+
 router.get("/singleJournal/:journalId", getSingleJournalController);
+
 router.get("/search", searchEntries);
 
 module.exports = router;
